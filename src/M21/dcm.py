f338b508e70b124c8882aae3dd44d3bcd0eb5bcf
Lennart Wilde <lennart-wilde@web.de>
2020-03-06 13:43:47 +0100

    Finished DCM Analysis

--- src/M21/dcm.py
+++ src/M21/dcm.py
@@ -11,11 +11,17 @@ from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap, Normalize
 import numpy as np
 
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+
 ########################################
 
 ## load data
 
 filenames = ["data/M21/DCM/spu1_3.dcmTileCoords", "data/M21/DCM/spu1_6.dcmTileCoords", "data/M21/DCM/spu2_3.dcmTileCoords", "data/M21/DCM/spu2_6.dcmTileCoords"]
+
+plot = False
 
 ## put it into a nice form
 dcm = list()
@@ -31,18 +37,52 @@ for name in filenames:
             x_list[x, y] = float(row[2])
     dcm.append(np.array(x_list))
 
-i = 1
+text = [(1, 1), (1, 2), (2, 1), (2, 2)]
+i = 0
+
+count_rates = np.empty((1))
+
 for plot_data in dcm:
-    pcm1 = plt.imshow(plot_data)
-    plt.colorbar(pcm1)
-    plt.title("DCM " + str(i) + "Linear skaliert")
-    plt.savefig("test" + str(i) + "_lin.eps")
-    plt.close()
 
-    pcm2 = plt.imshow(np.nan_to_num(np.log(plot_data + 0.000001), nan=0.00001))
-    plt.colorbar(pcm2)
-    plt.title("DCM " + str(i) + "Logarithmisch skaliert")
-    plt.savefig("test" + str(i) + "_log.eps")
-    plt.close()
+    if(plot):
+        pcm1 = plt.imshow(plot_data)
+        plt.colorbar(pcm1, label=r"Count Rate in $\frac{1}{s}$")
+        plt.title(r"DCM SPU " + str(text[i][0]) + ", Sensor " + str(text[i][1]) + " (Linear skaliert)")
+        plt.savefig("DCM_SPU" + str(text[i][0]) + "_Sensor" + str(text[i][1]) + "_lin.eps", bbox_inches = "tight")
+        plt.close()
 
-    i = i + 1
+        pcm2 = plt.imshow(np.nan_to_num(np.log(plot_data + 0.000001), nan=0.00001))
+        plt.colorbar(pcm2, label=r"Count Rate in $\ln(x)$")
+        plt.title(r"DCM SPU " + str(text[i][0]) + ", Sensor " + str(text[i][1]) + " Logarithmisch skaliert")
+        plt.savefig("DCM_SPU" + str(text[i][0]) + "_Sensor" + str(text[i][1]) + "_log.eps", bbox_inches = "tight")
+        plt.close()
+
+    count_rates = np.append(count_rates, plot_data.flatten())
+
+    i = i+1
+
+count_rates = count_rates[count_rates != -1]
+
+sorted_count_rates = np.sort(count_rates)
+
+sum = np.zeros(len(sorted_count_rates))
+marker = 0
+
+for i in range(2, len(sorted_count_rates)):
+
+    cut_array = sorted_count_rates[0:-i]
+    sum[i-2] = np.sum(cut_array)
+
+    if(marker == 0 and sum[i-2] < 0.5 * sum[0]):
+        marker = i
+
+
+fig = plt.figure(figsize=(16,9))
+plt.plot(sum, marker =".", linewidth = 0)
+plt.axvline(marker, color = "r", label=r"15$\%$ der Anfangsrate x = {}".format(marker))
+plt.xlabel(r"Anzahl Abgeschalteter SPADs", fontsize = 16)
+plt.ylabel(r"Dark Count Rate in $\frac{1}{s}$", fontsize = 16)
+plt.legend(fontsize = 16)
+
+plt.savefig("DCR_vs_N.png", bbox_inches="tight")
+plt.show()
