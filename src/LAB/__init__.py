4903491930c46aa77198c76554a61a7a675117c6
PhilippRo <philipp.rosendahl@gmx.de>
2020-03-15 17:19:15 +0100

    Create new module

---
+++ src/LAB/__init__.py
@@ -0,0 +1,22 @@
+#
+#
+# import imports
+#
+#
+
+import prelude
+
+sys.path.append(os.environ['PWD'])
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from src.common.fitting import *
+from src.common.plot_with_residuum import plot_with_residuum
+
+
+#
+# import submodules
+#
+#
+#
