f34f21caf11860562d62de18b631bed212f54ea5
PhilippRo <philipp.rosendahl@gmx.de>
2020-03-23 19:21:08 +0100

    Quicksave

--- src/T01/silicon.py
+++ src/T01/silicon.py
@@ -11,7 +11,7 @@ if not os.path.exists(ppath):
     os.makedirs(ppath)
 
 print("Stelle ein Spektrum da")
-counts = list(map(lambda x: int(x.strip()), open("data/T01/Experiment 1/1/spectrum_central.TKA").read().split("\n")[:-1]))
+counts = np.array(list(map(lambda x: int(x.strip()), open("data/T01/Experiment 1/1/spectrum_central.TKA").read().split("\n")[:-1])))
 plt.figure(figsize=(8,6), dpi=1200)
 plt.plot(np.array(list(range(len(counts)))) + 1, counts)
 plt.grid(True)
@@ -32,3 +32,24 @@ chi_sq:\t {}""".format(opt[0], np.sqrt(cov[0][0]), opt[1], np.sqrt(cov[1][1]), c
 print("Äquivalenzdicke: {} \pm {}".format(-1.0 * opt[1], np.sqrt(cov[1][1])))
 simple_figure(abs_energy, None, abs_pos, 0.05 * np.ones(len(abs_pos)), linf(abs_energy, *opt),
    "Lineary Shilding", "Energy MeV", "distance cm", "{}/liearyShielding.png".format(ppath))
+
+print("Detektorkalibrierunng")
+counts = list(map(lambda x: int(x.strip()), open("data/T01/Experiment 1/2/spectrum_closest.TKA").read().split("\n")[:-1]))
+plt.figure(figsize=(8,6), dpi=1200)
+plt.plot(np.array(list(range(len(counts)))) + 1, counts)
+plt.grid(True)
+plt.xlabel("Channel")
+plt.ylabel("Events")
+plt.title("Sample in closest postion")
+
+peak_energies = np.array([4.78, 5.4, 6.0, 7.69])
+channel_ranges = np.array([(250, 500), (600, 800), (950, 1050), (1600, 1800)])
+
+for (ener, (start, stop), col) in zip(peak_energies, channel_ranges, ['r', 'g', 'b', 'orange']):
+    plt.axvspan(start, stop, label="{} MeV Peak".format(ener), alpha=0.3, color=col)
+
+for (energy, (start, end)) in zip(peak_energies, channel_ranges):
+    opt, cov, chi_sq = do_normal_regression(np.arange(start, end, 1), counts[start:end])
+
+plt.savefig("{}/ClosestPostion.png".format(ppath))
+plt.close('all')
