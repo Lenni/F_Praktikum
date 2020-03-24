216f05d61873000025273c19ecb457445ba66f85
PhilippRo <philipp.rosendahl@gmx.de>
2020-03-24 18:02:36 +0100

    Quicksave

--- src/T01/silicon.py
+++ src/T01/silicon.py
@@ -85,31 +85,34 @@ simple_figure(mus, sigmas, peak_energies, np.zeros(len(mus)), linf(mus, *kalibMa
 #
 #
 #load data
-threshold = 50
+plt.close('all')
+threshold = 2
 shield_data = [list() for i in range(4)]
-for mes_dist in tqdm(chain(range(2070, 2400, 40), [2475])):
+for mes_dist in tqdm(chain(range(2150, 2400, 40), [2475])):
+    print(1)
     events = np.array(list(map(lambda x: float(x.strip()), open("data/T01/Experiment 1/4/spectrum_{}.TKA".format(mes_dist)).read().split("\n")[2:-1])))
     plt.figure(figsize=(3,4), dpi=200)
     plt.plot(list(range(len(events))), events)
+    upper_idx = 4093
+    print(len(events))
+    print(upper_idx)
+    while events[upper_idx] < 10 * threshold:
+        upper_idx = upper_idx - 1
+        print(upper_idx)
+    lower_idx = upper_idx
+    while events[lower_idx] > threshold:
+        lower_idx = lower_idx - 1
+    local_opt, local_cov, local_chi_sq = do_normal_regression(np.array(list(range(lower_idx, upper_idx))),
+        np.array(events[lower_idx:upper_idx]), np.array(np.sqrt(events[lower_idx:upper_idx])))
+    print("mittelwert: {} \pm {}, Fehler: {} \pm {}, chi_sq: {}".format(local_opt[0], local_opt[1],
+        np.sqrt(local_cov[0][0]), np.sqrt(local_cov[1][1]), chi_sq))
+    shield_data[0].append(25.1 - mes_dist / 100.0)
+    shield_data[1].append(np.sqrt(0.01 ** 2 + 0.1 ** 2))
+    shield_data[2].append(local_opt[0])
+    shield_data[3].append(local_opt[1])
+    plt.axvspan(lower_idx, upper_idx, alpha=0.6)
     plt.show()
     plt.close('all')
-    upper_idx = 4093
-#    print(len(events))
-#    print(upper_idx)
-#    while events[upper_idx] < 1.5 * threshold:
-#        upper_idx = upper_idx - 1
-#        print(upper_idx)
-#    lower_idx = upper_idx
-#    while events[lower_idx] > threshold:
-#        lower_idx = lower_idx - 1
-#    local_opt, local_cov, local_chi_sq = do_normal_regression(range(lower_idx, upper_idx),
-#        events[lower_idx:upper_idx], np.sqrt(events[lower_idx:upper_idx]))
-#    print("mittelwert: {} \pm {}, Fehler: {} \pm {}, chi_sq: {}".format(local_opt[0], local_opt[1],
-#        np.sqrt(local_cov[0][0]), np.sqrt(local_cov[1][1]), chi_sq))
-#    shield_data[0].append(25.1 - mes_dist / 100.0)
-#    shield_data[1].append(np.sqrt(0.01 ** 2 + 0.1 ** 2))
-#    shield_data[2].append(local_opt[0])
-#    shield_data[3].append(local_opt[1])
 
 #
 # Curry data like if it's were a delicious sausage with fries
@@ -121,9 +124,20 @@ distance_err = np.array(shield_data[1])
 channel = np.array(shield_data[2])
 channel_err = np.array(shield_data[3])
 
+print(shield_data)
+
 kalibDis, kalibDisErr, chi_sq = regression(linf, distance, channel, channel_err, xErr = distance_err)
 simple_figure(distance, distance_er, channel, channel_err, linf(distance, *kalibDis), "Distance Calibration, 7.69 MeV",
     "Distance /cm", "Channel", "{}/dis_calib.png".format(ppath))
 print(chi_sq)
+
+
+
+
+
+
+
+
+
 
 
