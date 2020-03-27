07c1e9c78c65e620f71521f0298754f5a78956dd
PhilippRo <philipp.rosendahl@gmx.de>
2020-03-27 23:22:18 +0100

    Ionisation Chamber analysis

--- src/T01/__main__.py
+++ src/T01/__main__.py
@@ -1,5 +1,5 @@
 
+import src.T01.ionisation_chamber
 import src.T01.beta_shielding
 import src.T01.scinti
-import src.T01.ionisation_chamber
 import src.T01.silicon
--- src/T01/ionisation_chamber.py
+++ src/T01/ionisation_chamber.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 import csv
 import os
 
+
+
 ppath = "protocols/T01/Plots/ionisation_chamber"
 if not os.path.exists(ppath):
     os.makedirs(ppath)
@@ -25,8 +27,9 @@ distance =np.array(distance)
 distance_err =np.array(distance_err)
 current =np.array(current)
 current_err =np.array(current_err)
-
-plt.errorbar(max(distance) - distance, current, current_err, distance_err, capsize=1, linewidth=2,
+distance = max(distance) - distance
+plt.figure(figsize=(8,6), dpi=1200)
+plt.errorbar(distance, current, current_err, distance_err, capsize=1, linewidth=2,
     elinewidth=1, color="r")
 plt.xlabel("Distance /cm")
 plt.ylabel("Current /mA")
@@ -36,9 +39,35 @@ plt.axvspan(3.0, 4.1, color='r', alpha=0.3, label="Plateau")
 x_0 = 5.0
 plt.axvline(x_0, color='blue', label="max. range {} cm".format(x_0))
 
-plt.legend()
 
 plt.grid(True)
+
+
+print("Maximale Höhe aus y Mittelwert der Plateaus")
+filter_ys = np.array([y for (x, y) in zip(distance, current) if 3.0 <= x and x<=4.1])
+y_max = sum(filter_ys)/len(filter_ys)
+print(filter_ys)
+y_max_err = sum(np.sqrt((filter_ys - y_max)**2 / (len(filter_ys)-1)))
+y_searched = y_max/2
+y_searched_err = 0
+
+m_tan = (current[-2] - current[-3])/(distance[-2] - distance[-3])
+m_tan_err= np.sqrt(((current_err[-3])**2 + (current_err[-2]**2))/(distance[-2] - distance[-3])**2 +((current[-2] - current[-3])/(distance[-2] - distance[-3])**2 * distance_err[-2])**2)
+c = current[-3] - m_tan*distance[-3]
+c_err = 0
+
+avr_rng = (y_searched - c)/m_tan
+avr_rng_err = np.sqrt((c/m_tan * y_searched_err)**2 + (y_searched/m_tan*c_err) **2+ ((y_searched - c)/m_tan**2 * m_tan_err)**2)
+
+plt.axhline(y_max, color='cyan', label="plateau level")
+plt.axhline(y_searched, color='orange', label="half of plateau")
+plt.axvline(avr_rng, color='orange', label="averange range")
+plt.plot(distance[-6:], m_tan*distance[-6:] + c, label="tangent line", alpha=0.6)
+
+plt.legend()
 plt.savefig("{}/CurrentToDistance.png".format(ppath))
 plt.close('all')
 
+print(avr_rng)
+print(avr_rng_err)
+print("mess max range: {:.3e} \pm {:.3e} cm".format(avr_rng, avr_rng_err))
